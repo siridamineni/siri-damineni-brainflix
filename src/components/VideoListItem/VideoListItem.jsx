bf02e8e09639ff689fa4df732d3420a30cc0acb2
Siri Damineni <siri.damineni@gmail.com>
2024-11-11 15:26:37 -0500

    bug/fix comments form display for responsive screens, prevent height increase in video list

--- src/components/VideoListItem/VideoListItem.jsx
+++ src/components/VideoListItem/VideoListItem.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import "./VideoListItem.scss";
 function VideoListItem({ src, videoName, videoBy }) {
   return (
-    <div className="video-list-item">
-      <img
-        className="video-list-item__thumnail-card"
-        src={src}
-        alt={videoName}
-      />
-      <div className="video-list-item__details">
+    <div className="list-item">
+      <div className="list-item__thumnail-card">
+        <img src={src} alt={videoName} />
+      </div>
+      <div className="list-item__details">
         <div>
-          <p className="video-list-item__name">{videoName}</p>
+          <p className="list-item__name">{videoName}</p>
         </div>
         <div>
-          <p className="video-list-item__maker-name">{videoBy}</p>
+          <p className="list-item__maker-name">{videoBy}</p>
         </div>
       </div>
     </div>
